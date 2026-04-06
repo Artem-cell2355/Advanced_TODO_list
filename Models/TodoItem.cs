2d89960345742562411f76eeb858a71a09fad2be
Artem-cell2355 <romanov.artem@stud.onu.edu.ua>
2026-04-07 02:19:20 +0300

    Добавьте файлы проекта.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,7 @@
+﻿namespace Advanced_TODO_list.Models; 
+
+public class TodoItem
+{
+    public string Title { get; set; } = "";
+    public bool IsDone { get; set; } = false;
+}
---
+++ Models/TodoList.cs
@@ -0,0 +1,9 @@
+﻿using Advanced_TODO_list.Models;
+
+namespace Advanced_TODO_list; 
+public class TodoList
+{
+    public string Name { get; set; } = "Новий список";
+
+    public List<TodoItem> Items { get; set; } = new();
+}
