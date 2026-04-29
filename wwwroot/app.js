0e78af03c33c25ed4a3c0f7c4dde478b4fa5726a
Artem-cell2355 <romanov.artem@stud.onu.edu.ua>
2026-04-29 21:15:34 +0300

    додав JS Interop и Cascading Value

---
+++ wwwroot/app.js
@@ -0,0 +1,9 @@
+﻿
+window.setJsonValue = (elementId, value) => {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.value = value;
+        element.style.backgroundColor = "#e8f0fe";
+        setTimeout(() => element.style.backgroundColor = "white", 500);
+    }
+};
